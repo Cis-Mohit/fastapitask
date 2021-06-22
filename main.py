8301c9341fd1a206c2f31ea3ddfe603289533ecc
neha <mohit.m@cisinlabs.com>
2021-06-22 18:12:57 +0530

    update db to insert values. add fake db query. add api to update balance, user details,and create user entery.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Main file to create api's."""
 import sqlite3
 from fastapi import FastAPI
 import datetime
@@ -5,15 +6,13 @@ from typing import Optional
 from pydantic import BaseModel
 
 app = FastAPI()
-import sqlite3
 
 
-
-conn = sqlite3.connect(':memory:') 
+conn = sqlite3.connect(':memory:')
 
 cur = conn.cursor()
 
-# Create User table
+
 cur.execute('''CREATE TABLE users
     ( user_id int AUTO_INCREMENT,
   username varchar(25)  NOT NULL UNIQUE,
@@ -22,15 +21,16 @@ cur.execute('''CREATE TABLE users
   password varchar(30) NOT NULL,
   bitcoinAmount float(20) NOT NULL,
   usdBalance float(20) NOT NULL,
-  createdAt timestamp NOT NULL,
+  createdAt timestamp NULL,
   updatedAt timestamp NULL,
   PRIMARY KEY (user_id)
   );''')
 
+
 # Create Crypto table.
 cur.execute('''CREATE TABLE crypto
 ( name varchar(25) PRIMARY KEY,
-  
+
   price float(20) NOT NULL,
   updatedAt timestamp NULL
 
@@ -41,196 +41,276 @@ conn.commit()
 
 # Closing Connection.
 # conn.close()
-tables = list(cur.execute("select name from sqlite_master where type is 'table'"))
-print(tables)
+# tables = list(cur.execute("select name from sqlite_master where type is 'table'"))
+# print(tables)
+
+# dummy data.
+cur.execute("""INSERT INTO users (user_id, name, username, email, password, bitcoinAmount, usdBalance)
+        VALUES (1,'joy', 'cooljoy', 'joy@gmail.com', 'joy123', 0, 0)""")
+
+cur.execute("""INSERT INTO crypto (name, price, updatedAt)
+        VALUES ('bitcoin', 100, '2021-01-04T00:12:01.000Z')""")
+
+conn.commit
 
 
 class User(BaseModel):
+    """User model to get data to functon."""
+
+    user_id:Optional[int]
     name: str
     username: str
     email: str
     password: str
     createdAt: Optional[str] = None
-    bitcoinAmount:Optional[float] = 0.0
-    usdBalance:Optional[float] = 0.0
+    bitcoinAmount: Optional[float] = 0.0
+    usdBalance: Optional[float] = 0.0
     updatedAt: Optional[str] = None
 
 
-@app.get('/')
-async def test():
-    return {'msg': 'hello'}
+class Crypto(BaseModel):
+    """Crypto model to get data to functon."""
+
+    name: str
+    price: Optional[float] = 100.0
+
+
+class UpdateUsdBalance(BaseModel):
+    """Model to update data to functon."""
+
+    action_type: str
+    balance: float
+
+
+class UpdateBitcoinBalance(BaseModel):
+    """Model to get data to functon."""
+
+    action_type: str
+    balance: float
+
 
 @app.post("/users")
 async def signup(user: User):
-    # {
-    # “name”: “Jon A”,
-    # “username”: “jonjon”,
-    # “email”: “jon@jmail.com”
-    # }
-    # conn = sqlite3.connect(':memory:') 
-    global cur
+    """Api to signup user."""
     if user.name:
-
         name = user.name
     else:
-        return {'status':False, 'code':404, 'msg': 'Name field required.'}
+        return {'status': False, 'code': 404, 'msg': 'Name field required.'}
     if user.username:
 
         username = user.username
     else:
-        return {'status':False, 'code':404, 'msg': 'Username field required.'}
+        return {'status': False, 'code': 404, 'msg': 'Username field required.'}
 
     if user.email:
 
         email = user.email
     else:
-        return {'status':False, 'code':404, 'msg': 'EMAIL field required.'}
-    if user.password:
+        return {'status': False, 'code': 404, 'msg': 'EMAIL field required.'}
 
+    if user.password:
         password = user.password
     else:
-        return {'status':False, 'code':404, 'msg': 'Password field required.'}
+        return {'status': False, 'code': 404, 'msg': 'Password field required.'}
+    if user.user_id:
+        user_id = user.user_id
+    else:
+        return {'status': False, 'code': 404, 'msg': 'user_id field required.'}
+    global cur
     cur = conn.cursor()
     current_time = datetime.datetime.now().timestamp()
     # Insert a row of data
 
-    tables = list(cur.execute("select name from sqlite_master where type is 'table'"))
-    print(tables)
+    # tables = list(cur.execute("select name from sqlite_master where type is 'table'"))
+    # print(tables)
 
+    cur.execute("""INSERT INTO users (user_id, name, username, email, password, bitcoinAmount, usdBalance, createdAt)
+        VALUES (?,?, ?, ?, ?, 0, 0, ?)""", (user_id, name, username, username, password, current_time))
 
-    cur.execute("""INSERT INTO users (name, username, email, password, bitcoinAmount, usdBalance, createdAt)
-        VALUES ('{name}', '{username}' ,'{username}' ,'{password}' ,0 ,0 ,'{current_time}');""")
 
     # Save (commit) the changes
     conn.commit()
+    user_data = cur.execute("SELECT * FROM users WHERE username=?;", (username,)).fetchone()
+    user_collumn = ('user_id', 'name', 'username', 'email', 'password', 'bitcoinAmount', 'usdBalance',
+                    'createdAt', 'updatedAt')
+    response = dict(zip(user_collumn, user_data))
 
-    # We can also close the connection if we are done with it.
-    # Just be sure any changes have been committed or they will be lost.
-    conn.close()
+    # conn.close()
 
-    response = {
-    'id':1,
-    'bitcoinAmount':0,
-    'usdBalance':0,
-    'createdAt':'',
-    'updatedAt':'',
-    'name': 'Jon A',
-    'username': 'jonjon',
-    'email': 'jon@jmail.com'
-        }
+    response = {'response': response}
 
     return response
 
 
 @app.get("/users/{id}")
-async def fetch_user():
+async def fetch_user(id):
+    
+    global cur
+    user_data = cur.execute("SELECT * FROM users WHERE user_id=?;", (id,)).fetchone()
+    user_collumn = ('user_id', 'name', 'username', 'email', 'password', 'bitcoinAmount', 'usdBalance',
+                    'createdAt', 'updatedAt')
+    response = dict(zip(user_collumn, user_data))
 
-    # SELECT * FROM USERS WHERE id=id;
-    response = {
-    'id':1,
-    'bitcoinAmount':0,
-    'usdBalance':0,
-    'createdAt':'',
-    'updatedAt':'',
-    'name': 'Jon A',
-    'username': 'jonjon',
-    'email': 'jon@jmail.com'
-    }
+    response = {'response': response}
+
     return response
+
 
 @app.put("/users/{id}")
-async def update_user():
-    # {
-    # “name”: “Jon A”,
-    # “email”: “jon@jmail.com”
-    # }
-    response = {
-    'id':1,
-    'bitcoinAmount':0,
-    'usdBalance':0,
-    'createdAt':'',
-    'updatedAt':'',
-    'name': 'Jon A',
-    'username': 'jonjon',
-    'email': 'jon@jmail.com'
-    }
+async def update_user(user: User):
+    """Fuction to update user details."""
+    global cur
+    user_id = id
+    if user.name:
+        name = user.name
+    else:
+        return {'status': False, 'code': 404, 'msg': 'Name field required.'}
+
+    if user.email:
+        email = user.email
+    else:
+        return {'status': False, 'code': 404, 'msg': 'EMAIL field required.'}
+
+    current_time = datetime.datetime.now().timestamp()
+
+    cur.execute(""" UPDATE users SET name=?, email=?, updatedAt=? WHERE user_id=?;""", (
+        name, email, current_time, user_id))
+    # Save (commit) the changes.
+    conn.commit()
+    user_data = cur.execute("SELECT * FROM users WHERE user_id=?;", (id,)).fetchone()
+    user_collumn = ('user_id', 'name', 'username', 'email', 'password', 'bitcoinAmount', 'usdBalance',
+                    'createdAt', 'updatedAt')
+    response = dict(zip(user_collumn, user_data))
+
+    response = {'response': response}
+
     return response
-
-
 
 
 @app.post("/users/{id}/usd")
-async def user_usd_balance_update():
-    # {
-    # “action”: “withdraw” or “deposit”,
-    # “amount”: 40.05
-    # }
-    response = {
-    'id':1,
-    'bitcoinAmount':0,
-    'usdBalance':0,
-    'createdAt':'',
-    'updatedAt':'',
-    'name': 'Jon A',
-    'username': 'jonjon',
-    'email': 'jon@jmail.com'
-    }
+async def user_usd_balance_update(usdbal: UpdateUsdBalance):
+    """Fuction to sell and buy bitcoins."""
+    global cur
+    user_bal = cur.execute("SELECT usdBalance  FROM users WHERE user_id=?;", (id,)).fetchone()
+    usdbalance = user_bal[0]
+
+    if usdbal.action_type:
+        action_type = usdbal.action_type
+    else:
+        return {'status': False, 'code': 404, 'msg': 'Type field required.'}
+
+    if usdbal.balance:
+        balance = usdbal.balance
+    else:
+        return {'status': False, 'code': 404, 'msg': 'Balance field required.'}
+    if action_type == '“deposit”':
+        updated_balance = usdbalance + balance
+
+    elif action_type == '“withdraw”':
+        if usdbalance < balance:
+            return {'status': False, 'code': 404, 'msg': 'Not Sufficient balance'}
+        else:
+            updated_balance = usdbalance - balance
+
+    else:
+        return {'status': False, 'code': 404, 'msg': 'Enter valid type field.'}
+
+    current_time = datetime.datetime.now().timestamp()
+    cur.execute(""" UPDATE users SET usdBalance=?, updatedAt=? WHERE name='bitcoin';""", (
+        updated_balance, current_time))
+    # Save (commit) the changes.
+    conn.commit()
+    user_bal = cur.execute("SELECT usdBalance  FROM users WHERE user_id=?;", (id,)).fetchone()
+    usdbalance = user_bal[0]
+
+    response = {'usdbalance': usdbalance}
     return response
 
 @app.post("/users/{id}/bitcoins")
-async def user_bitcoins_balance_update():
-    # {
-    # “action”: "buy" or “sell”,
-    # “amount”: 40.05
-    # }
-    response = {
-    'id':1,
-    'bitcoinAmount':0,
-    'usdBalance':0,
-    'createdAt':'',
-    'updatedAt':'',
-    'name': 'Jon A',
-    'username': 'jonjon',
-    'email': 'jon@jmail.com'
-    }
+async def user_bitcoins_balance_update(bitcoinbal: UpdateBitcoinBalance):
+    """Fuction to sell and buy bitcoins."""
+    user_bal = cur.execute("SELECT bitcoinAmount  FROM users WHERE user_id=?;", (id,)).fetchone()
+    bitcoinbalance = user_bal[0]
+
+    if bitcoinbal.action_type:
+        action_type = bitcoinbal.action_type
+    else:
+        return {'status': False, 'code': 404, 'msg': 'Type field required.'}
+
+    if bitcoinbal.balance:
+        balance = bitcoinbal.balance
+
+    else:
+        return {'status': False, 'code': 404, 'msg': 'Balance field required.'}
+    if action_type == 'buy':
+        updated_balance = bitcoinbalance + balance
+
+    elif action_type == 'sell':
+        if bitcoinbalance < balance:
+            return {'status': False, 'code': 404, 'msg': 'Not Sufficient balance'}
+        else:
+            updated_balance = bitcoinbalance - balance
+
+    else:
+        return {'status': False, 'code': 404, 'msg': 'Enter valid type field.'}
+
+    current_time = datetime.datetime.now().timestamp()
+    cur.execute(""" UPDATE users SET bitcoinAmount=?, updatedAt=? WHERE name='bitcoin';""", (
+        updated_balance, current_time))
+
+    # Save (commit) the changes.
+    conn.commit()
+    user_bal = cur.execute("SELECT usdBalance,bitcoinAmount  FROM users WHERE user_id=?;", (id,)).fetchone()
+    bitcoinbalance = user_bal[0]
+
+    response = {'bitcoinbalance': bitcoinbalance}
     return response
 
 
 @app.get("/users/{id}/balance")
-async def get_user_balance():
-    # {
-    # “action”: "bubalr “sell”,
-    # “amount”: 40.05
-    # }
-    balance = usd + bitcoin * 32100
-    response = {
-    'balance': balance,
-    
-    }
+async def get_user_balance(id):
+    """Get User current balance."""
+    global cur
+    user_bal = cur.execute("SELECT usdBalance,bitcoinAmount  FROM users WHERE user_id=?;", (id,)).fetchone()
+    crypto = cur.execute("SELECT price  FROM crypto WHERE name='bitcoin';").fetchone()
+    bitcoin_price = crypto[0]
+    usdbal = user_bal[0]
+    bitcoinbal = user_bal[1]
+    total_bal = usdbal + bitcoinbal * bitcoin_price
+
+    response = {'balance': total_bal}
     return response
+
 
 @app.get("/bitcoin")
-async def fetch_bitcoin():
-    # {
-    # “price”: 100.00,
-    # }
+async def fetch_bitcoin(crypto: Crypto):
+    """Get bitcoin current price."""
+    global cur
+    bitcoin = cur.execute("SELECT price, updatedAt  FROM crypto WHERE name='bitcoin';").fetchone()
     response = {
-    'price': 100.00,
-    'updatedAt' :'2021-01-04T00:12:01.000Z'
+        'price': bitcoin[0],
+        'updatedAt': bitcoin[1]
     }
     return response
+
 
 @app.put("/bitcoin")
-async def update_bitcoin():
-    # {
-    # “price”: 100.00,
-    # }
+async def update_bitcoin(crypto: Crypto):
+    """Api to update bitcoin price."""
+    global cur
+    if crypto.price:
+        price = crypto.price
+    else:
+        return {'status': False, 'code': 404, 'msg': 'Price field required.'}
+    current_time = datetime.datetime.now().timestamp()
+    cur.execute(""" UPDATE crypto SET price, updatedAt=? WHERE name='bitcoin';""", (
+        price, current_time))
+
+    # Save (commit) the changes.
+    conn.commit()
+    bitcoin = cur.execute("SELECT price, updatedAt  FROM crypto WHERE name='bitcoin';").fetchone()
     response = {
-    'price': 100.00,
-    'updatedAt': '2021-01-04T00:12:01.000Z'
+        'price': bitcoin[0],
+        'updatedAt': bitcoin[1]
     }
     return response
-
-
-
-
